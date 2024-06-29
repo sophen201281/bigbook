34455b2c5c8f2e0a2079487d215bf92bc98c90da
sophen201281 <phansophen@gmail.com>
2024-06-29 12:13:56 +0700

    first commit

---
+++ bb/files/js/action.js
@@ -0,0 +1,119 @@
+// Functions Chair - Page 01
+var PageOne_1 = document.getElementById('PageOne').innerHTML  = 'ច..';
+var PageOne_2 = document.getElementById('SubPageOne').innerHTML  = 'ច..';
+$('#PageOne').css('background-color', '#FDD017');
+$('#SubPageOne').css('background-color', '#FDD017');
+var pageone = PageOne_1 + PageOne_2;
+function PageOne(id) {
+    if (pageone == 1) {
+        document.getElementById("PageOne").innerHTML = "ច.. ";
+        $('#PageOne').css('background-color', '#FDD017');
+        document.getElementById("SubPageOne").innerHTML = "ច.. ";
+        $('#SubPageOne').css('background-color', '#FDD017');
+        pageone = 0;
+    }
+
+    else {
+        document.getElementById("PageOne").innerHTML = "ចាប";
+        $('#PageOne').css('background-color', 'white');
+        document.getElementById("SubPageOne").innerHTML = "ចាប";
+        $('#SubPageOne').css('background-color', 'white');
+        pageone = 1;
+    }
+}
+
+//Functions Chair - Page 02
+var PageTwo_1 = document.getElementById('PageTwo').innerHTML  = 'រ..';
+var PageTwo_2 = document.getElementById('SubPageTwo').innerHTML  = 'រ..';
+$('#PageTwo').css('background-color', '#FDD017');
+$('#SubPageTwo').css('background-color', '#FDD017');
+var pagetwo = PageTwo_1 + PageTwo_2;
+function PageTwo(id) {
+    if (pagetwo == 1) {
+        document.getElementById("PageTwo").innerHTML = "រ.. ";
+        $('#PageTwo').css('background-color', '#FDD017');
+        document.getElementById("SubPageTwo").innerHTML = "រ.. ";
+        $('#SubPageTwo').css('background-color', '#FDD017');
+        pagetwo = 0;
+    }
+
+    else {
+        document.getElementById("PageTwo").innerHTML = "រុយ";
+        $('#PageTwo').css('background-color', 'white');
+        document.getElementById("SubPageTwo").innerHTML = "រុយ";
+        $('#SubPageTwo').css('background-color', 'white');
+        pagetwo = 1;
+    }
+}
+
+//Functions Chair - Page 03
+var PageThree_1 = document.getElementById('PageThree').innerHTML  = 'ក..';
+var PageThree_2 = document.getElementById('SubPageThree').innerHTML  = 'ក..';
+$('#PageThree').css('background-color', '#FDD017');
+$('#SubPageThree').css('background-color', '#FDD017');
+var pagethree = PageThree_1 + PageThree_2;
+function PageThree(id) {
+    if (pagethree == 1) {
+        document.getElementById("PageThree").innerHTML = "ក.. ";
+        $('#PageThree').css('background-color', '#FDD017');
+        document.getElementById("SubPageThree").innerHTML = "ក.. ";
+        $('#SubPageThree').css('background-color', '#FDD017');
+        pagethree = 0;
+    }
+
+    else {
+        document.getElementById("PageThree").innerHTML = "កុក";
+        $('#PageThree').css('background-color', 'white');
+        document.getElementById("SubPageThree").innerHTML = "កុក";
+        $('#SubPageThree').css('background-color', 'white');
+        pagethree = 1;
+    }
+}
+
+//Functions Chair - Page 04
+var PageFour_1 = document.getElementById('PageFour').innerHTML  = 'ទ..';
+var PageFour_2 = document.getElementById('SubPageFour').innerHTML  = 'ទ..';
+$('#PageFour').css('background-color', '#FDD017');
+$('#SubPageFour').css('background-color', '#FDD017');
+var pagefour = PageFour_1 + PageFour_2;
+function PageFour(id) {
+    if (pagefour == 1) {
+        document.getElementById("PageFour").innerHTML = "ទ.. ";
+        $('#PageFour').css('background-color', '#FDD017');
+        document.getElementById("SubPageFour").innerHTML = "ទ.. ";
+        $('#SubPageFour').css('background-color', '#FDD017');
+        pagefour = 0;
+    }
+
+    else {
+        document.getElementById("PageFour").innerHTML = "ទីទុយ";
+        $('#PageFour').css('background-color', 'white');
+        document.getElementById("SubPageFour").innerHTML = "ទីទុយ";
+        $('#SubPageFour').css('background-color', 'white');
+        pagefour = 1;
+    }
+}
+
+//Functions Chair - Page 05
+var PageFive_1 = document.getElementById('PageFive').innerHTML  = 'ម..';
+var PageFive_2 = document.getElementById('SubPageFive').innerHTML  = 'ម..';
+$('#PageFive').css('background-color', '#FDD017');
+$('#SubPageFive').css('background-color', '#FDD017');
+var pagefive = PageFive_1 + PageFive_2;
+function PageFive(id) {
+    if (pagefive == 1) {
+        document.getElementById("PageFive").innerHTML = "ម.. ";
+        $('#PageFive').css('background-color', '#FDD017');
+        document.getElementById("SubPageFive").innerHTML = "ម.. ";
+        $('#SubPageFive').css('background-color', '#FDD017');
+        pagefive = 0;
+    }
+
+    else {
+        document.getElementById("PageFive").innerHTML = "មូស";
+        $('#PageFive').css('background-color', 'white');
+        document.getElementById("SubPageFive").innerHTML = "មូស";
+        $('#SubPageFive').css('background-color', 'white');
+        pagefive = 1;
+    }
+}
